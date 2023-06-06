a9e9671d0c0453e43d08247693d5e7f61f0a8220
Vizal Fepwarmen <vizal@inforsys.co.id>
2023-06-06 17:24:30 +0700

    navbar toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from'@angular/material/toolbar';
+import { MatSidenavModule } from'@angular/material/sidenav';
+import { MatListModule } from'@angular/material/list';
+import { MatIconModule } from'@angular/material/icon';
+import { MatButtonModule } from'@angular/material/button';
+import { FlexLayoutModule } from'@angular/flex-layout';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
